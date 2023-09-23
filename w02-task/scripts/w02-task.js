41c7719ba068cdc25d02e73ec322b44936574753
MaylekArthur98 <Maylek.arthur@gmail.com>
2023-09-23 11:34:22 -0400

    k

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -1,20 +1,24 @@
 /* W02-Task - Profile Home Page */
 
 /* Step 1 - Setup type tasks - no code required */
-
 /* Step 2 - Variables */
-
-
+let fullName = "Maylek Arthur";
+let currentYear = '2023';
+let profilePicture = 'images/image.jpg';
 
 
 /* Step 3 - Element Variables */
-
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
 
 
 
 /* Step 4 - Adding Content */
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = `<strong>${currentYear}</strong>`;
+imageElement.setAttribute('scr', profilePicture);
 
 
 
