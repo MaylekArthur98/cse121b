b66f810a2eba2005dfb2ee2ed4d4dfc69d484f00
MaylekArthur98 <Maylek.arthur@gmail.com>
2023-10-07 16:23:24 -0400

    k

---
+++ scripts/w04-task.js
@@ -0,0 +1,33 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+    name: "Maylek Arthur",
+    photo: profilePicture,
+    
+
+};
+
+
+
+/* Populate Profile Object with placesLive objects */
+
+
+
+
+/* DOM Manipulation - Output */
+
+/* Name */
+
+/* Photo with attributes */
+
+
+/* Favorite Foods List*/
+
+
+/* Hobbies List */
+
+
+/* Places Lived DataList */
+
+
