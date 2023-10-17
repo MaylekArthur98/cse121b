9457e7030640fe585689d579f693c3a470bc0f59
MaylekArthur98 <Maylek.arthur@gmail.com>
2023-10-17 13:25:54 -0400

    k

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -1,11 +1,16 @@
 /* W05: Programming Tasks */
 
 /* Declare and initialize global variables */
-const templesElement = 
-
+const templesElement = document.querySelector("#temples");
+let templeslist = {};
 /* async displayTemples Function */
-
-
+const displayTemples = (temples) => {
+    temples.array.forEach(temple => {
+        let article = document.createElement('article');
+        let h3 = document.createElement('h3');
+        temples.templeName.inner.HTML
+    });
+};
 
 
 /* async getTemples Function using fetch()*/
