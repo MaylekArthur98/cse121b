bd46f9cf65928db00cff775aa96a8c3922479230
MaylekArthur98 <Maylek.arthur@gmail.com>
2023-10-17 14:48:06 -0400

    k

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -2,31 +2,38 @@
 
 /* Declare and initialize global variables */
 const templesElement = document.querySelector("#temples");
-let templeslist = {};
+let templelist = {};
 /* async displayTemples Function */
 const displayTemples = (temples) => {
-    temples.array.forEach(temple => {
+    temples.forEach(temple => {
         let article = document.createElement('article');
         let h3 = document.createElement('h3');
-        h3 = temple.templeName;
+        h3.textContent = temple.templeName;
         let img = document.createElement('img');
-        img.src = temple.imangeURL;
+        img.src = temple.imageURL;
         img.alt = temple.location;
-
+        article.appendChild(h3);
+        article.appendChild(img);
+        templesElement.appendChild(article);
     });
 };
 
 
 /* async getTemples Function using fetch()*/
-
+const getTemples = async () => {
+    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+    templelist = await response.json();
+    displayTemples(templelist);
+    //console.log('TEMPLE DATA:', templelist);
+}
 
 /* reset Function */
+getTemples();
 
 
 /* sortBy Function */
 
 
 
-getTemples();
 
 /* Event Listener */
